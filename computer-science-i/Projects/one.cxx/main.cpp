562c447b91969fe493081cb41d69825dbf35a0f1
Shannon Poulsen <129866040+spoulsen05@users.noreply.github.com>
2024-11-04 12:33:03 -0500

    Update main.cpp

--- computer-science-i/Projects/one.cxx/main.cpp
+++ computer-science-i/Projects/one.cxx/main.cpp
@@ -4,7 +4,7 @@ Name: Shannon Poulsen
 Project: Test 1
 Class: CMPS-147 / Computer Science I
 Section: T/F 9:55 AM - 11:35 AM / 01
-Email: spoulsen@ramapo.edu
+Email:
 Date: 6 October 2023
 
 *******************************************************************************/
