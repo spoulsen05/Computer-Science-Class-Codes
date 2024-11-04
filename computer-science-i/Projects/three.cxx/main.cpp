0451934bcf53ab81e8bcffc20ffafd830857d327
Shannon Poulsen <129866040+spoulsen05@users.noreply.github.com>
2024-11-04 12:36:03 -0500

    Update main.cpp

--- computer-science-i/Projects/three.cxx/main.cpp
+++ computer-science-i/Projects/three.cxx/main.cpp
@@ -1,10 +1,10 @@
 /******************************************************************************
 
-Name: Severin Poulsen
+Name: Shannon Poulsen
 Project: Test 3
 Class: CMPS-147 / Computer Science I
 Section: T/F 9:55 AM - 11:35 AM / 01
-Email: spoulsen@ramapo.edu
+Email:
 Date: 15 December 2023
 
 *******************************************************************************/
