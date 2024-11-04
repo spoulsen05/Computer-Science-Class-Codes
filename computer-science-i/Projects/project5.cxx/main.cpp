d666879881d7a4fba05f367f5b1bba9c6add8a85
Shannon Poulsen <129866040+spoulsen05@users.noreply.github.com>
2024-11-04 12:34:56 -0500

    Update main.cpp

--- computer-science-i/Projects/project5.cxx/main.cpp
+++ computer-science-i/Projects/project5.cxx/main.cpp
@@ -1,10 +1,10 @@
 /******************************************************************************
 
-Name: Severin Poulsen
+Name: Shannon Poulsen
 Project: Project 5 / Fortune Teller
 Class: CMPS-147 / Computer Science I
 Section: T/F 9:55 AM - 11:35 AM / 01
-Email: spoulsen@ramapo.edu
+Email: 
 Date: 10 October 2023
 
 *******************************************************************************/
